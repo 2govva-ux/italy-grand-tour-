fbbf9678764cf75e0def1cd0fb84ce1fe3baf377
2govva-ux <2govva@gmail.com>
2026-07-22 12:24:36 +0300

    Update app.js
    Connect itinerary to map

--- css/js/app.js
+++ css/js/app.js
@@ -1,1 +1,27 @@
+const buttons = document.querySelectorAll(".timeline button");
+const itineraryBox = document.getElementById("itinerary");
 
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const dayNumber = Number(button.dataset.day);
+
+        const day = itinerary.find(item => item.day === dayNumber);
+
+        if (!day) return;
+
+        itineraryBox.innerHTML = `
+            <h2>Day ${day.day} – ${day.city}</h2>
+            <h3>${day.title}</h3>
+
+            <ul>
+                ${day.activities.map(a => `<li>${a}</li>`).join("")}
+            </ul>
+        `;
+
+        map.setView(day.coordinates, 12);
+
+    });
+
+});
